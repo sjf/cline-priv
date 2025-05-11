9d28abc6d117bf50ee404a11e5cac65ef7d079c4
Sarah Fortune <fortunes@cline.bot>
2025-05-11 04:35:24 +0100

    Stop using google-protobuf npm module to generate JS for protos
    The code generated by google-protobuf cannot serialize protos from plain objects. It needs the protos to be class instances created with ProtoExample.create().
    But, the protos created in the extension are just POJOs.
    Use protoLoader instead which is fine with plain objects.
    Protoloader is also the method used in the grpc JS documentation: https://grpc.io/docs/languages/node/basics/#loading-service-descriptors-from-proto-files

---
+++ standalone/build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -ux
+
+BUILD_DIR=build
+
+if [ ! -f ../dist/extension.js ]; then
+  echo You need to build the cline extension before running this script.
+  exit 1
+fi
+
+mkdir -p $BUILD_DIR 2>/dev/null || true
+
+# Copy the standalone files into the buld dir.
+cp -av files/. $BUILD_DIR
+
+PROTO_DIR=../proto
+# The proto files are needed when using protoLoader and gRPC reflection.
+cp -av $PROTO_DIR/*.proto $BUILD_DIR
+
+# Copy the pre-built extension
+cp ../dist/extension.js $BUILD_DIR
+echo 'module.exports.Controller = Controller' >> build/extension.js
+
+# Generate gRPC server for the services and handlers.
+node generate-server.js
+
+# Install npm modules used by the extension at runtime.
+cd $BUILD_DIR
+npm install
+
+if find node_modules -name '*.node' | grep .; then
+  echo Native node modules are being used. Build cannot proceed.
+  exit 1
+fi
+
+# Zip all the files needed for the standalone extension.
+zip -q -r standalone.zip . -x standalone.zip
+
